6fc392a31e08cce12072c4473e20f5681b68b45d
Asaf Baibekov <asafall21@gmail.com>
2022-10-29 13:50:26 +0300

    Initial Commit

---
+++ Cryptography/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Cryptography
+//
+//  Created by Asaf Baibekov on 29/10/2022.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	printf("Hello, World!\n");
+	return 0;
+}
